e6163888fd956b20349c99c1bf0303f1a3152136
VladtheImplier <vlad@vladevpayne.xyz>
2025-04-09 22:14:28 -0400

    Update plugin.rb
    no instructions

--- plugin.rb
+++ plugin.rb
@@ -3,35 +3,6 @@
 # version: 0.1
 # authors: vladtheimplier
 
-# -------------------------
-# Install this theme component:
-#
-# api.onPageChange(async () => {
-#   const isAdmin = window.location.pathname.startsWith("/admin");
-#   const existing = document.getElementById("discourse-node-info");
-#
-#   if (isAdmin) {
-#     if (!existing) {
-#       const nodeName = await getNodeName();
-#       const nodeInfo = document.createElement("div");
-#       nodeInfo.id = "discourse-node-info";
-#       nodeInfo.className = "alert alert-info";
-#       nodeInfo.style.position = "fixed";
-#       nodeInfo.style.bottom = "1em";
-#       nodeInfo.style.right = "1em";
-#       nodeInfo.style.zIndex = "10000";
-#       nodeInfo.style.maxWidth = "300px";
-#
-#       nodeInfo.textContent = `Node: ${nodeName}`;
-#       document.body.appendChild(nodeInfo);
-#     }
-#   } else {
-#     if (existing) {
-#       existing.remove();
-#     }
-#   }
-# });
-
 after_initialize do
   module ::DiscourseNodeInfo
     class Engine < ::Rails::Engine
